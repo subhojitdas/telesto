b280aeadc90bfa6767bf975d771c8ffd4ca8cf1e
subhojit das <subhojitdss@gmail.com>
2025-08-18 01:00:00 +0530

    Transformer encoder block

--- src/vit/vit_encoder.py
+++ src/vit/vit_encoder.py
@@ -61,3 +61,29 @@ class MLP(nn.Module):
         x = self.fc2(x)
         x = self.dropout(x)
         return x
+
+
+class TransformerEncoderBlock(nn.Module):
+    def __init__(
+            self,
+            dim,
+            num_heads,
+            mlp_ratio=4.0,
+            attention_dropout=0.1,
+            projection_dropout=0.1,
+            qkv_bias=True,
+    ):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(dim, eps=1e-6)
+        self.attn = MultiHeadSelfAttention(dim,
+                           num_heads=num_heads,
+                           attention_dropout=attention_dropout,
+                           projection_dropout=projection_dropout
+                    )
+        self.norm2 = nn.LayerNorm(dim, eps=1e-6)
+        self.mlp = MLP(fan_in=dim, hidden_dim=int(dim * mlp_ratio), dropout=projection_dropout)
+
+    def forward(self, x):
+        x = x + self.attn(self.norm1(x))
+        x = x + self.mlp(self.norm2(x))
+        return x
